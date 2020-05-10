a172b73b6dba50958789c96761d1b5b213961849
Anik Das <anikdas0811@gmail.com>
2020-05-10 15:58:35 +0530

    Fix day10.py, forgot to save again -_-

--- May30DayChallenge/day10.py
+++ May30DayChallenge/day10.py
@@ -1,6 +1,20 @@
-p = {}
-# dictionary to store how many people trust i
-N = 4
-for i in range(1, N+1):
-    p[i] = 0
-print(p)
+class Solution:
+    def findJudge(self, N: int, trust: List[List[int]]) -> int:
+        p = {}
+        # dictionary to store how many people trust i
+        # N = 4
+        for i in range(1, N+1):
+            p[i] = 0
+        # print(p)
+        for pair in trust:
+            # pair[0] trusts pair[1]
+            a = pair[0]
+            b = pair[1]
+            p[a] -= 1
+            p[b] += 1
+            
+        for i in range(1, N+1):
+            if p[i] == N-1:
+                return i
+        return -1
+            
